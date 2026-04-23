04c0b269cff76e6c9d2221e9b7f89e45e1d5a304
Racca04 <ryanracca12@gmail.com>
2026-04-23 16:23:14 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -16,3 +16,57 @@ setInterval(() => {
   document.getElementById("seconds").innerText =
     Math.floor((diff / 1000) % 60);
 }, 1000);
+
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function addToCart(nome, preco, imagem) {
+  cart.push({ nome, preco, imagem });
+  localStorage.setItem("cart", JSON.stringify(cart));
+  updateCart();
+}
+
+function removeItem(index) {
+  cart.splice(index, 1);
+  localStorage.setItem("cart", JSON.stringify(cart));
+  updateCart();
+}
+
+function updateCart() {
+  const cartItems = document.getElementById("cart-items");
+  const cartCount = document.getElementById("cart-count");
+  const cartTotal = document.getElementById("cart-total");
+
+  cartItems.innerHTML = "";
+  let total = 0;
+
+  cart.forEach((item, index) => {
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+      <div class="cart-item">
+        <img src="${item.imagem}" class="cart-img">
+        
+        <div class="cart-info">
+          <p>${item.nome}</p>
+          <span>R$ ${item.preco}</span>
+        </div>
+
+        <button class="remove-btn" onclick="removeItem(${index})">
+          ✖
+        </button>
+      </div>
+    `;
+
+    cartItems.appendChild(li);
+    total += item.preco;
+  });
+
+  cartCount.innerText = cart.length;
+  cartTotal.innerText = "Total: R$ " + total.toFixed(2);
+}
+
+function toggleCart() {
+  document.getElementById("cart").classList.toggle("open");
+}
+
+updateCart();
